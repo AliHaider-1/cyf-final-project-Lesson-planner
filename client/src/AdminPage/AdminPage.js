9aa3f1fa0f12a61c1b138ef2fb45f3bd40cce9a0
Patrick Chan <q_p18@hotmail.com>
2021-01-14 06:38:48 +0000

    Tidy code. Change 'class' to 'lesson'

--- client/src/AdminPage/AdminPage.js
+++ client/src/AdminPage/AdminPage.js
@@ -6,54 +6,48 @@ import Footer from "../Footer/Footer";
 import Admin_Page from"../Images/Admin_Page.jpg";
 import { Link } from "react-router-dom";
 
- const lessons = fakeLessons.lessons;
+const lessons = fakeLessons.lessons;
 const AdminPage =({RolesButtonHandler})=>{
  
+  return(
+    <div>
+      <Header />
 
-    return(
-      <div>
-<Header />
-
-    <img className="imgAdmin d-inline" src={Admin_Page}></img>
+      <img className="imgAdmin d-inline" src={Admin_Page}></img>
 
       <div class="text-right createClass d-inline">
-      <Link to="/CreateClassPage">
-       <button class="btn btn-danger">
-         Create Class
-       </button>
-     </Link>
-  </div>
+        <Link to="/CreateClassPage">
+          <button class="btn btn-danger">
+            Create Lesson
+          </button>
+        </Link>
+      </div>
 
       <div className="ULPage">
         <h3 className="text-left mt-0"><strong>Signed Volunteers</strong></h3>
-  <table>
-    <thead>
-    <tr>
-      <th>No.</th>
-      <th>Module</th>
-      <th>Lesson</th>
-      <th>Date</th>
-      <th>Time</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    {lessons.map(el =>
-  
-<VolunteersTable el={el} RolesButtonHandler={RolesButtonHandler}/>
-  
-  
-     
-  )}
-  </tbody>
-  </table>
-</div>  
-<Footer />
-      </div>
-      
-    
-    );
+        <table>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Module</th>
+              <th>Lesson</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {lessons.map(el =>
+        
+              <VolunteersTable el={el} RolesButtonHandler={RolesButtonHandler}/>
+        
+            )}
+          </tbody>
+        </table>
+      </div>  
+      <Footer />
+    </div>    
+  );
 }
-
 
 export default AdminPage;
