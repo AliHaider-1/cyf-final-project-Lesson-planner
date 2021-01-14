eac078374303431dcbdb2478b206ef67a5c1a3b4
Patrick Chan <q_p18@hotmail.com>
2021-01-14 06:40:44 +0000

    Tidy code

--- client/src/AdminPage/CreateClassPage.js
+++ client/src/AdminPage/CreateClassPage.js
@@ -7,21 +7,17 @@ import Admin_Page from"../Images/Admin_Page.jpg";
 import AvailableRoles from "../LessonPage/AvailableRoles";
 import CreateRoles from "./CreateRoles";
 
- const lessons = fakeLessons.lessons;
+const lessons = fakeLessons.lessons;
 const CreateClassPage =({RolesButtonHandler})=>{
  
-
-    return(
-      <div>
-    <Header />
-    <img className="imgAdmin d-inline" src={Admin_Page}></img>
-    <CreateRoles/>
-    <Footer />
-      </div>
-      
-    
-    );
+  return(
+    <div>
+      <Header />
+      <img className="imgAdmin d-inline" src={Admin_Page}></img>
+      <CreateRoles/>
+      <Footer />
+    </div>    
+  );
 }
-
 
 export default CreateClassPage;
