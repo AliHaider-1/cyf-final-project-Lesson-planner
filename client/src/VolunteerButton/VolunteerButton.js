f61af1a40d649fa7a41f1e46a30ea0a607d9d1e9
Ali Haider <alihaiderx736@gmail.com>
2021-01-16 02:35:53 +0000

    - volunteer button added

---
+++ client/src/VolunteerButton/VolunteerButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const VolunteerButton = () => {
+  return (
+    <div>
+      <strong className="btn btn-danger nav_Volunteer">Sign Up For Class</strong>
+    </div>
+  );
+};
+export default VolunteerButton;
